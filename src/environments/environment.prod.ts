5df9ddaef9e279695e72915dd54fcdb1eb0a9cbc
kw <kw@tebkom.at>
2021-11-22 15:57:20 +0100

    Change purchased item name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     PAYPAL_CLIENT_ID: 'AVGrO4Qr2il2mkNjP-igenDR7sUsZPIuiVBsqS5YkNjhJYeJMLv-_dnLEG1dUMFjNWgT7GU0l-4EpfHz',
     PAYPAL_CURRENCY: 'EUR',
     // Suffixed by size ' <n>x<n>', e.g. '40x40'
-    PAYPAL_ITEM_NAME: 'Fotoleinwand AllesGute.info',
+    PAYPAL_ITEM_NAME: 'Fotoleinwand',
     // Prices for different canvas sizes
     // First price is 'statt', 2nd is real price sent to paypal
     CANVAS_PRICES: {
